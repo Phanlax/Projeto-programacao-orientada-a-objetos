9df991185a70de97965ce940f5332632457c4448
Gustavo de Oliveira Santos <gustavo.os170205@gmail.com>
2026-05-09 21:41:43 -0300

    commit com a versão completamente funcional
    provavelmente não tem nada novo

---
+++ main/java/proj/projetofeitv/Usuario.java
@@ -0,0 +1,48 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package proj.projetofeitv.model;
+
+/**
+ *
+ * @author Gustavo
+ */
+public class Usuario {
+    private int id;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+    private String usuario;
+    private String senha;       
+
+    
+    public Usuario(String usuario, String senha){
+        this.usuario = usuario;
+        this.senha = senha;
+    }
+    
+ 
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+    
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+
+
+    public String getSenha() {
+        return senha;
+    }
+}
